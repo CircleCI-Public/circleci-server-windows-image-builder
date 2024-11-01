89dc534614fdff42a372460598119850a466042c
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-01 12:10:51 -0500

    Revert changes

--- windows2022/provision-scripts/ansible-helper.bat
+++ windows2022/provision-scripts/ansible-helper.bat
@@ -1,3 +1,3 @@
 choco install -y python3 --version=3.11.4
 
-@REM choco install -y visualstudio2019community --version=16.11.28.0 --allWorkloads --includeRecommended --includeOptional --quiet --locale=en-US --force
+choco install -y visualstudio2019community --version=16.11.28.0 --allWorkloads --includeRecommended --includeOptional --quiet --locale=en-US --force
