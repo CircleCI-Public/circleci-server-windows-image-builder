00f279c660dd2e45d736d00aa0494b493b50f22f
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-01 12:16:00 -0500

    Fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,13 +28,13 @@ resource "aws_security_group" "windows_sg" {
     from_port = "3389"
     to_port ="3389"
     protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = "0"
     to_port ="0"
     protocol = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
